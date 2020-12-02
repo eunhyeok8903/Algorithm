cdd67f3eef928774c624a0ac790f0dd7107859b9
eunhyeok8903 <eunhyeok8903@gmail.com>
2020-12-03 02:43:42 +0900

    .

---
+++ code/etc/BestTImeTpBuy.py
@@ -0,0 +1,22 @@
+def maxProfit(prices):
+    size = len(prices)
+    dp = [[0] * size for i in range(2)]
+    dp[1][0] = prices[0] * (-1)
+    print(dp)
+    for i in range(1, size):
+        print(i)
+        dp[0][i] = max(dp[0][i - 1], dp[1][i - 1] + prices[i])
+        dp[1][i] = max(dp[1][i - 1], dp[0][i - 1] - prices[i])
+        print(dp)
+    return max(dp[0][len(prices) - 1], dp[1][len(prices) - 1])
+
+
+"""
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+문제링크
+
+주식 사고팔기 문제
+dp[0][i]=max(dp[1][i-1]+prices[i],dp[0][i-1]) //이번에 판경우, 안사고 그대로 온 경우
+dp[1][i]=max(dp[0][i-1]+prices[i],dp[1][i-1]) //이번에 산경우, 안팔고 그대로 온 경우
+
+"""
--- main.py
+++ main.py
@@ -1,32 +1,7 @@
-from collections import deque
-n,m=map(int,input().split(' '))
-graph=[]
-for _ in range(n):
-    graph.append(list(map(int,input())))
-dr=[0,0,1,-1]
-dc=[1,-1,0,0]
-visited=[[False]*m for _ in range(n)]
-result=999999
+s="aabbbaab"
 
-def bfs():
-    q=deque()
-    q.append((0,0,1))
-    visited[0][0]=True
-
-    while q:
-        r,c,cnt=q.popleft()
-        print(r,c,cnt)
-        global result
-        if r==n-1 and c==m-1 :
-            result=min(result,cnt)
-            return result
-        for i in range(4):
-            nr=r+dr[i]
-            nc=c+dc[i]
-
-            if nr>=0 and nc>=0 and nr<n and nc<m:
-                if visited[nr][nc]==False and graph[nr][nc]==1:
-                    visited[nr][nc]=True
-                    q.append((nr,nc,cnt+1))
-bfs()
-print(result)
+b=s.replace("aa","cc")
+print(s)
+print(b)
+d=s.replace("aa","cc",1)
+print(d)
