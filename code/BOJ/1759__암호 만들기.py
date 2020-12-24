7c7bf7e8f8a3555914b41f3ff20e43d3a0284b46
eunhyeok8903 <eunhyeok8903@gmail.com>
2020-12-24 15:38:43 +0900

    12/24

--- code/BOJ/1759__암호 만들기.py
+++ code/BOJ/1759__암호 만들기.py
@@ -3,30 +3,18 @@ li=list(input().split())
 li.sort()
 
 from itertools import combinations
-c=list(combinations(li,L))
+c=combinations(li,L)
 for i in c:
-    arr=list(i)
     cnt=0
-    for ch in arr:
+    for ch in i:
         if ch=='a' or ch=='e' or ch=='i' or ch=='u' or ch=='o':
             cnt+=1
     if cnt>=1 and L-cnt>=2:
-        print(''.join(arr))
+        print(''.join(i))
     else:
         continue
 
 # from itertools import combinations,permutations
 # a=[1,2,3]
 # p=permutations(a,2)     //튜플형태 반환
-# c=combinations(a,2)
-"""
-from itertools import combinations,permutations
-
-사전순 조합->정렬후 조합
-combinations나 permutations는 튜플형태 반환이라서
-c= list(combinations())로 받으면
-[(a,b,c), (a,c,b),(b,a,c) ] 이런꼴이다.
-
-for tu in c: 로 
-각 튜플을 다시 list로 만들어 사용하던지 a,b,c=tu로 받아서 사용하던지 
-"""
+# c=permutations(a,2)
--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-L,C=map(int,input().split())
-li=list(input().split())
-li.sort()
+from sys import stdin
+S1=stdin.readline().rstrip()
+S2=stdin.readline().rstrip()
 
-from itertools import combinations
-c=list(combinations(li,L))
-for i in c:
-    arr=list(i)
-    cnt=0
-    for ch in arr:
-        if ch=='a' or ch=='e' or ch=='i' or ch=='u' or ch=='o':
-            cnt+=1
-    if cnt>=1 and L-cnt>=2:
-        print(''.join(arr))
-    else:
-        continue
+len1=len(S1)
+len2=len(S2)
 
-# from itertools import combinations,permutations
-# a=[1,2,3]
-# p=permutations(a,2)     //튜플형태 반환
-# c=permutations(a,2)
+dp=[[0]*(len2+1) for _ in range(len1+1)]
+
+for i in range(1,len1+1):
+    for j in range(1,len2+1):
+        if S1[i-1]==S2[j-1]:
+            dp[i][j]=dp[i-1][j-1]+1
+        else:
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+
+print(dp[-1][-1])
+
+"""
+문자 같을 때
+dp[i][j]=dp[i-1][j-1]+1
+두글자가 추가되기전 가장 큰 길이에서 +1
+AB AC 에서 이미 최고 큰 수가 1로 정해졌으니까 당연히 여기서 +1 된게 ABD ACD에서 최장임
+
+다를 때
+이전꺼나 위에꺼에서 가장 큰 수 가져옴
+
+
+#잔기술
+dp[1][1]부터 시작하는 이유는 제일처음 문자가 참조할게 없을때 dp[1][0]참조하게하여 for문안에 dp식으로만 끝나게함
+이거안하면 새롭게 조건 달아주거나 따로 또 잔 작업 해줘야함
+
+"""
