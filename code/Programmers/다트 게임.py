20fd9c9103030c741b882d1165d73c41594e7021
eunhyeok <eunhyeok8903@gmail.com>
2021-01-22 10:14:30 +0900

    # 1/22

---
+++ code/Programmers/다트 게임.py
@@ -0,0 +1,90 @@
+# def solution(dartResult):
+#     length=len(dartResult)
+#     li=[]; s=0; e=0
+#     for i in range(length):
+#         if dartResult[i]=='#' or dartResult[i]=='*':
+#             continue
+#
+#         if dartResult[i]>='A' and dartResult[i]<='Z':
+#             if i==length-1:
+#                li.append(dartResult[s:i+1])
+#             else:
+#                 if dartResult[i+1]=='#' or dartResult[i+1]=='*':
+#                     e=i+1
+#                     li.append(dartResult[s:e+1])
+#                 else:
+#                     e=i
+#                     li.append(dartResult[s:e+1])
+#                 s=e+1
+#     result=[]
+#     for s in li:
+#         l=len(s)
+#         idx=1
+#         if s[idx]=='0':
+#             idx=2
+#         n=int(s[:idx])
+#
+#         #배수 처리
+#         if s[idx]=='D':
+#             n=n**2
+#         elif s[idx]=='T':
+#             n=n**3
+#         result.append(n)
+#
+#         #옵션 처리
+#         if s[-1]=='*':
+#             if len(result)==1:
+#                 result[-1]*=2
+#             else:
+#                 result[-1]*=2
+#                 result[-2]*=2
+#         elif s[-1]=='#':
+#             result[-1]*=-1
+#     answer=0
+#     for s in result:
+#         answer+=s
+#     return answer
+
+import re
+
+def solution(dartResult):
+    bonus = {'S' : 1, 'D' : 2, 'T' : 3}
+    option = {'' : 1, '*' : 2, '#' : -1}
+    p = re.compile('(\d+)([SDT])([*#]?)')
+    dart = p.findall(dartResult)
+    for i in range(len(dart)):
+        if dart[i][2] == '*' and i > 0:
+            dart[i-1] *= 2
+        dart[i] = int(dart[i][0]) ** bonus[dart[i][1]] * option[dart[i][2]]
+
+    answer = sum(dart)
+    return answer
+
+"""
+문제 
+3번 다트를 던진다
+1S2D*3T //1S 1점, 2D* 2^2 에 이전꺼 포함 x2 , 3T 3^3점 총 37점
+1T2D#3D# //1T 1^3점 , 2D# 2^2해당점수 x(-1) , 3D# 3^2*(-1) 총 -4점 
+"""
+
+"""
+문제접근
+
+간단한 구현문제다.
+파싱을 어떻게 할지가 중요한데, 표현식을 이용한 파싱을 생각했지만
+문법이 익숙하지 않아서 for문으로 풀어 비효율적이었다.
+"""
+
+"""
+문제 풀이
+
+for문으로 풀경우 코드가 직관적이지도 않고 실수할 확률이 높아졌다.
+
+정규표현식 문법
+https://wikidocs.net/4308
+
+p = re.compile('(\d+)([SDT])([*#]?)')
+\d는 숫자와 매치 [0-9] 의미, +는 최소 한번이상 반복 의미
+[SDT] S,D,T 중에서 나온다
+[*#]?  *,#이 나올수도 있고 안나올수도 있다.
+"""
--- main.py
+++ main.py
@@ -1,32 +1,7 @@
-# from sys import stdin
-# def solution():
-#     global s
-#     idx=0
-#     target_idx=0
-#     for i in range(len-1,0,-1):
-#         if s[i]>s[i-1]:
-#             idx=i
-#             break
-#     target_idx=idx; target_result=s[idx]
-#     for i in range(idx,len):
-#         if s[idx-1]<s[i] and target_result>=s[i]:
-#             target_idx=i
-#             target_result=s[i]
-#
-#     s[idx-1],s[target_idx]=s[target_idx],s[idx-1]
-#     s=s[:idx]+sorted(s[idx:])
-#     for ch in s:
-#         print(ch,end=' ')
-#
-# N=int(input())
-# s= map(int,stdin.readline().rstrip().split())
-# s=list(s)
-# len=len(s)
-# if s==sorted(s,reverse=True):
-#     print(-1)
-# else:
-#     solution()
-#
-li=['a','b','c']
-s=str(li)
-print(s)
+import re
+
+# p=re.compile('[a-z]+')
+# p = re.compile('(\d+)([SDT])([*#]?)')
+p = re.compile('(\d+)([SDT])([*#]?)')
+result = p.findall('a bca abab acb')
+print(result)
